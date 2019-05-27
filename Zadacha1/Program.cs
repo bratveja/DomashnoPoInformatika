a07ff9d38c627dc02c0bb6eb1ff5c74ec952c550
bratveja <bratvejaxs@mail.bg>
2019-05-27 22:17:48 +0300

    Add files via upload

---
+++ Zadacha1/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace Zadacha1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pal = Console.ReadLine();
+            Console.WriteLine(Palindrom(pal));
+        }
+
+        private static bool Palindrom(string pal)
+        {
+            int start = 0;
+            int end = pal.Length - 1;
+            for (int i = 0; i < pal.Length / 2; i++)
+            {
+                if (pal[start] != pal[end])
+                {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+            return true;
+        }
+    }
+}
---
+++ Zadacha1/README.md
@@ -0,0 +1,4 @@
+﻿Zadacha1 - стр. 30 зад. 12
+
+
+Atanas Ivanov
